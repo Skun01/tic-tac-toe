df4c3bf43bf8144f6219032c188d2d41253383a7
Skun01 <truongg9655@gmail.com>
2024-06-25 18:10:07 +0700

    make events controller js

---
+++ assets/js/playingMode.js
@@ -0,0 +1,9 @@
+const PlayingMode = (function(){
+    const playingMode = document.querySelector('.playing-mode');
+    const humanMode = document.querySelector('.human-mode');
+    const botMode = document.querySelector('.bot-mode');
+
+    function humanModeChoosing(){
+        
+    }
+})();
